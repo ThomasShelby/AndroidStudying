98c9d0116e611184266429c27ebd4dd2a4475ffe
Thomas Shelby <mikola1693@gmail.com>
2015-06-30 21:42:22 +0300

    Two_Buttons_And_Click_Listener

--- src/com/example/MyFirstProject/MyActivity.java
+++ src/com/example/MyFirstProject/MyActivity.java
@@ -3,11 +3,13 @@ package com.example.MyFirstProject;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MyActivity extends Activity {
 
     private TextView textView;
+    private Button button;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -15,6 +17,14 @@ public class MyActivity extends Activity {
         setContentView(R.layout.main);
 
         textView= (TextView) findViewById(R.id.textView);
+        button= (Button) findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Hello Everyone!!!");
+            }
+        });
     }
 
     public void sayHello(View view){
