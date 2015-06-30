47fb1388e1b93ba811fc98d68e0cd002cee101fc
Thomas Shelby <mikola1693@gmail.com>
2015-06-30 16:08:52 +0300

    Button+clickOnButton

--- src/com/example/MyFirstProject/MyActivity.java
+++ src/com/example/MyFirstProject/MyActivity.java
@@ -2,14 +2,22 @@ package com.example.MyFirstProject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MyActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
+    private TextView textView;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        textView= (TextView) findViewById(R.id.textView);
+    }
+
+    public void sayHello(View view){
+        textView.setText("Hello!!!");
     }
 }
